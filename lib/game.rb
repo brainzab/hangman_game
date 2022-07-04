006388dd1a5fb4f6b40120f3c4214daf243a2b2a
Kirill <107773834+brainzab@users.noreply.github.com>
2022-07-05 02:03:07 +0300

    Update game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -2,12 +2,12 @@ class Game
   TOTAL_ERRORS_ALLOWED = 7
 
   def initialize(word)
-    @letters = word.chars
+    @letters = word.upcase.chars
     @user_guesses = []
   end
 
   def errors
-    @user_guesses - @letters
+    @user_guesses - normalized_letters
   end
 
   def errors_made
@@ -19,14 +19,25 @@ class Game
   end
 
   def letters_to_guess
-    result = @letters.map do |letter|
-      letter if @user_guesses.include?(letter)
+    @letters.map do |letter|
+      letter if @user_guesses.include?(normalize_letter(letter))
     end
-    result
   end
 
   def lost?
     errors_allowed.zero?
+  end
+
+  def normalize_letter(letter)
+    case letter
+    when 'Ё' then 'Е'
+    when 'Й' then 'И'
+    else letter
+    end
+  end
+
+  def normalized_letters
+    @letters.map { |letter| normalize_letter(letter) }
   end
 
   def over?
@@ -34,26 +45,16 @@ class Game
   end
 
   def play!(letter)
-    @user_guesses << letter if !over? && !@user_guesses.include?(letter)
+    normalize_letter = normalize_letter(letter)
+
+    @user_guesses << normalize_letter unless over? && @user_guesses.include?(normalize_letter)
   end
 
   def won?
-    (@letters - @user_guesses).empty?
+    (normalized_letters - @user_guesses).empty?
   end
 
   def word
     @letters.join
-  end
-
-  def normalize_letter(letter)
-    case letter
-    when "Ё" then "Е"
-    when "Й" then "И"
-    else letter
-    end
-  end
-
-  def normalized_letters
-    @letters.map { |letter| normalize_letter(letter) }
   end
 end
